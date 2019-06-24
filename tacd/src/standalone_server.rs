e403cc9b4cd11a78362d73dd2938763d6c3e4a25
Rodolphe Breard <rodolphe@what.tf>
2019-06-24 18:50:34 +0200

    Update tacd standalone server

--- tacd/src/standalone_server.rs
+++ tacd/src/standalone_server.rs
@@ -1,10 +1,10 @@
-use acme_common::crypto::{PrivateKey, X509Certificate};
+use acme_common::crypto::{KeyPair, X509Certificate};
 use acme_common::error::Error;
 
 pub fn start(
     listen_addr: &str,
     certificate: &X509Certificate,
-    private_key: &PrivateKey,
+    key_pair: &KeyPair,
 ) -> Result<(), Error> {
     Err("The standalone server is not implemented yet.".into())
 }
