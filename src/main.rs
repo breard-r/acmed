016d7e0d1b9e037f66241b7f588a08b8633fb32f
Rodolphe Bréard <v.code.ngyvz7vf@what.tf>
2024-12-21 12:38:04 +0100

    Async

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+pub const APP_THREAD_NAME: &str = "acmed-runtime";
+
+fn main() {
+	tokio::runtime::Builder::new_multi_thread()
+		.enable_all()
+		.thread_name(APP_THREAD_NAME)
+		.build()
+		.unwrap()
+		.block_on(start());
+}
+
+async fn start() {
+}
