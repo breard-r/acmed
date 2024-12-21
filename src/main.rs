2376d83d017fa7a5e9e60b7bf7817ec7c8e267a2
Rodolphe Bréard <v.code.ngyvz7vf@what.tf>
2024-12-21 13:52:08 +0100

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
 pub const APP_THREAD_NAME: &str = "acmed-runtime";
 
 fn main() {
+	let subscriber = FmtSubscriber::builder()
+		.with_max_level(Level::TRACE)
+		.finish();
+	tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 	tokio::runtime::Builder::new_multi_thread()
 		.enable_all()
 		.thread_name(APP_THREAD_NAME)
@@ -10,4 +17,5 @@ fn main() {
 }
 
 async fn start() {
+	tracing::info!("Starting ACMEd.");
 }
